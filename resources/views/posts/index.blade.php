279302ce1d6b4b9dbcbb6e3d75ec487ce0fe61b0
Paul Standley <paulstandley1972@gmail.com>
2019-07-01 19:57:30 +0100

    display posts

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Post</h1>
+    @if(count($posts) > 0)
+        @foreach($posts as $post)
+            <div class="well">
+            <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                <small>Witten on the {{$post->created_at}}</small>
+            </div>
+        @endforeach
+        @else
+        <p>No Posts found</p>
+    @endif
+@endsection
