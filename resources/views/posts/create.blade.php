3201c559b35f6c6d0477ce86bf5feee75c45b39e
Paul Standley <paulstandley1972@gmail.com>
2019-07-03 14:42:02 +0100

    create post form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,9 +2,15 @@
 
 @section('content')
     <h1>Create Post</h1>
-    <form action="{{ action('PostController@store') }}" method="POST">
-      <input name="title" type="text" value="{{ csrf_token() }}" >
-      
-    </form>  
-
+    {!! Form::open(['action' => 'PostController@store', 'method' => 'POST']) !!}
+        <div class="form-group">
+            {{ Form::label('title', 'Title') }}
+            {{ Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title']) }}
+        </div>
+        <div class="form-group">
+            {{ Form::label('body', 'Body') }}
+            {{ Form::textarea('body', '', ['class' => 'form-control', 'placeholder' => 'Body Text']) }}
+        </div>
+        {{ Form::submit('submit', ['class' => 'btn btn-primary']) }}
+    {!! Form::close() !!}
 @endsection
