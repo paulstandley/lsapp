d0803c4909ea3dcb0cd09bf3fdbc6518cd403053
Paul Standley <paulstandley1972@gmail.com>
2019-07-06 17:38:18 +0100

    delete post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <a href="/posts" class="btn btn-default">Go Back</a>
+    <a href="/posts" class="btn btn-primary">Go Back</a>
     <h1>{{$post->title}}</h1>
     <div>
       {!!$post->body!!}
@@ -9,5 +9,9 @@
     <hr>
     <small>Witten on the {{$post->created_at}}</small>
     <hr>
-    <a href="/posts/{{$post->id}}/edit" class="btn btn-default">Edit</a>
+    <a href="/posts/{{$post->id}}/edit" class="btn btn-success">Edit</a>
+    {!! Form::open(['action' => ['PostController@destroy', $post->id], 'method' => 'POST', 'class' => 'pull-right']) !!}
+    {{Form::hidden('_method', 'DELETE')}}
+    {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+    {!! Form::close() !!}
 @endsection
