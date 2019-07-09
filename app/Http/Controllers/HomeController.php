fbadd869ae9c117d8066b60a6d8dc27548c8e336
Paul Standley <paulstandley1972@gmail.com>
2019-07-09 19:56:17 +0100

    stash

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,7 +24,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user_id = auth()->user('id');
+        $user_id = auth()->user()->id;
         $user = User::find($user_id);
         //var_dump($user);
         return view('home');
